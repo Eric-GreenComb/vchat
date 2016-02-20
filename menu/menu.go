59d5a5cd8a46eb79f5f8dbc28b22a3f1cd9da9ba
eric <ministor@126.com>
2016-02-21 00:39:08 +0800

    add flag

--- menu/menu.go
+++ menu/menu.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/banerwai/vchat/util"
 	"github.com/chanxuehong/wechat/mp"
-	"github.com/chanxuehong/wechat/mp/menu"
 	"github.com/pelletier/go-toml"
+	"io/ioutil"
+	"net/url"
 )
 
+var AppDir string
+var _app_dir string
 var TomlTree *toml.TomlTree
 var CorpId, CorpSecret string
 
@@ -14,7 +19,11 @@ var AccessTokenServer *mp.DefaultAccessTokenServer
 var MpClient *mp.Client
 
 func init() {
-	TomlTree, _ = toml.LoadFile("config.toml")
+	flag.StringVar(&_app_dir, "d", "default", "input app dir")
+	flag.Parse()
+	AppDir = fmt.Sprintf("./%s/", _app_dir)
+
+	TomlTree, _ = toml.LoadFile(AppDir + "config.toml")
 
 	CorpId = TomlTree.Get("vchat.corpId").(string)
 	CorpSecret = TomlTree.Get("vchat.corpSecret").(string)
@@ -24,33 +33,34 @@ func init() {
 }
 
 func main() {
-	var subButtons1 = make([]menu.Button, 3)
-	subButtons1[0].SetAsViewButton("每日新文", "http://green-comb.com/")
-	subButtons1[1].SetAsViewButton("往日新文", "http://www.baidu.com/")
-	subButtons1[2].SetAsClickButton("关于我们", "company")
-
-	var subButtons2 = make([]menu.Button, 5)
-	subButtons2[0].SetAsViewButton("This推荐", "http://green-comb.com/")
-	subButtons2[1].SetAsViewButton("This推荐2", "http://www.baidu.com/")
-	subButtons2[2].SetAsViewButton("推荐3", "http://www.baidu.com/")
-	subButtons2[3].SetAsViewButton("推荐4", "http://www.baidu.com/")
-	subButtons2[4].SetAsViewButton("推荐5", "http://www.baidu.com/")
-
-	var subButtons3 = make([]menu.Button, 3)
-	subButtons3[0].SetAsViewButton("greencomb", "http://green-comb.com/")
-	subButtons3[1].SetAsViewButton("搜索", "http://www.baidu.com/")
-	subButtons3[2].SetAsViewButton("APP", "http://ssskdjf.com/download/aaa.apk")
-
-	var mn menu.Menu
-	mn.Buttons = make([]menu.Button, 3)
-	mn.Buttons[0].SetAsSubMenuButton("水调新文", subButtons1)
-	mn.Buttons[1].SetAsSubMenuButton("水调推荐", subButtons2)
-	mn.Buttons[2].SetAsSubMenuButton("Customer", subButtons3)
-
-	menuClient := (*menu.Client)(MpClient)
-	if err := menuClient.CreateMenu(mn); err != nil {
+	if err := CreateMenu(AppDir + "menu.json"); err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println("set menu success")
+	fmt.Println(_app_dir + " set menu success")
+}
+
+func CreateMenu(menu_file string) (err error) {
+	var result mp.Error
+
+	incompleteURL := "https://api.weixin.qq.com/cgi-bin/menu/create?access_token="
+
+	_f, _ := ioutil.ReadFile(menu_file)
+
+	_token, err := MpClient.Token()
+	if err != nil {
+		return
+	}
+
+	finalURL := incompleteURL + url.QueryEscape(_token)
+
+	if err = util.PostRawJson(finalURL, _f, &result); err != nil {
+		return
+	}
+
+	if result.ErrCode != mp.ErrCodeOK {
+		err = &result
+		return
+	}
+	return
 }
